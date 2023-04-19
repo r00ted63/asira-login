6b9129f639b6ed5fc60797a2794844f45a484fd8
Mason H <126316676+r00ted63@users.noreply.github.com>
2023-04-18 20:46:57 -0400

    Add files via upload

---
+++ asiranews/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  asiranews
+//
+//  Created by Mason James Hood on 4/18/23.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginClicked(_ sender: UIButton) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { firebaseresult, error in
+            if let e = error {
+                print("error")
+            }
+            else {
+                self.performSegue(withIdentifier: "fromlogin", sender: self)
+            }
+            
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
